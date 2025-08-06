c8c3ce1b47ccdd2c25951c60f7de916d75a84f89
Victor Vechi <victorhvechi@gmail.com>
2025-08-06 15:11:11 -0300

    set eventContext

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { EventModule } from './events/event.module';
 import { PrismaService } from './database/prisma.service';
 import { AccountRepository } from './account/domain/repository/account-repository';
 import { PrismaAccountRepository } from './account/infrastructure/repository/prisma-account-repository';
 import { EventController } from './events/infrastructure/controller/event.controller';
 import { AccountService } from './account/application/account.service';
+import { EventContextService } from './events/application/event-context.service';
+import { EventContextInterface } from './events/domain/application/event-context-interface';
 
 @Module({
   imports: [],
   controllers: [
     EventController,
-    AppController
+    AppController,
   ],
   providers: [
     PrismaService,
     AccountService,
     {
+      provide: EventContextInterface,
+      useClass: EventContextService
+    },
+    {
       provide: AccountRepository,
       useClass: PrismaAccountRepository
     }
-
   ],
 })
 export class AppModule {}
