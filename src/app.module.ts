d2ca6b2cb24f33f9465b03a2e8978ed16da5e1ad
Victor Vechi <victorhvechi@gmail.com>
2025-08-06 10:09:16 -0300

    create event module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventModule } from './events/event.module';
+import { PrismaService } from './database/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [EventModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [PrismaService],
 })
 export class AppModule {}
